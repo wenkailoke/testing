4096cbc8f8843cdb56289437d35bda1509e34acb
hyiik09 <107246141+hyiik09@users.noreply.github.com>
2022-09-03 23:39:34 +0800

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,9 @@
+#include <stdlib.h>
+#include <string.h>
+
+const char *hello(void)
+{
+    char *ans = strdup("Hello, World!");
+    /* string is pointer of the first character */
+    return ans;
+}
